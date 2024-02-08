93be8de0cb606dba970bb9e2279e400dfb2c8cfb
kartik <kartikb.5578@gmail.com>
2024-02-08 14:15:56 +0530

    event data updated

--- src/components/RSVP/data.js
+++ src/components/RSVP/data.js
@@ -1,32 +1,28 @@
 export const data = [
     {
         id: 1,
-        title: "Event 1",
-        date: "2021-10-10",
+        title: "The Chip Chase Quest: Build & Boot",
+        date: "2024-14-2",
         time: "10:00",
-        location: "Location 1",
+        location: "A wing, DY Patil College of Engineering, Akurdi",
         description:
-            "lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet",
+            "Prepare for a pulse-pounding CPU odyssey! Join us in constructing and awakening the heartbeat of computing power. This immersive quest delves deep into the world of CPU technology, challenging teams to decode cryptic clues and strategically assemble components. 🧩💻",
         rules: [
-            "lorem ipsum dolor sit amet",
-            "lorem ipsum dolor sit amet",
-            "lorem ipsum dolor sit amet",
-            "lorem ipsum dolor sit amet",
+            "Team Size 3 ",
+            "Fast Team to compelete Wins",
         ],
         contact: [
-            "lorem ipsum dolor sit amet",
-            "lorem ipsum dolor sit amet",
-            "lorem ipsum dolor sit amet",
-            "lorem ipsum dolor sit amet",
+            "Kartik Bankar - 8554876161 ",
+            "DJ - 8010174858",
         ],
-        info: "lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet",
+        info: "This is CPU building challenge",
     },
     {
         id: 2,
-        title: "Event 2",
-        date: "2021-10-10",
+        title: "Toy Language Saga!🌟",
+        date: "2024-14-2",
         time: "10:00",
-        location: "Location 2",
+        location: "A wing, DY Patil College of Engineering, Akurdi",
         description:
             "lorem ipsum dolor sit amet lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet",
         rules: [
